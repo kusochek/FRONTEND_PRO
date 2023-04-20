4b5bcbc1a156e96d5da0a30d2bf462d1ac8b7809
kusochek <kusochek1995@gmail.com>
2023-04-20 09:47:08 +0200

    dirst

---
+++ 1/index.js
@@ -0,0 +1,43 @@
+console.log('Hello world!');
+console.log(10);
+
+// STRING
+// NUMBER
+// NULL
+// UNDEFINED
+// BOOLEAN
+
+vasya = 100;
+petya = '5';
+
+// console.log(Vasya, Petya, 'Some another value!', 10, 20, Vasya);
+// console.log(Petya);
+
+petyaCurrentNumberOfApple = petya + 5;
+vasyaCurrentNumberOfApple = vasya - 5;
+
+console.log(petyaCurrentNumberOfApple);
+console.log(vasyaCurrentNumberOfApple);
+
+firstName = 'Solomiia';
+lastName = 'Kasimova';
+
+fullName = lastName + ' ' + firstName;
+
+console.log(fullName);
+
+// alert('Some message here!');
+
+// age = prompt('How old are you?');
+// alert(age);
+// answer = 'I\'m' + ' ' + age + ' years old!';
+// console.log(answer);
+
+// isHappy = confirm('Are you happy?');
+// console.log(isHappy);
+
+currentYear = 2023;
+yearOfBirth = prompt('What is your birth year?');
+
+userAge = currentYear + yearOfBirth;
+console.log(userAge);
