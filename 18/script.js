6f116fab36c1fd2010f84c4cb40c960c1b281ef8
Oleksandra Dontsova <oleksandra.dontsova@danads.se>
2023-07-05 20:19:05 +0200

    lesson 18

---
+++ 18/script.js
@@ -0,0 +1,39 @@
+// cut, copy, paste
+
+// const firstName = document.querySelector('#firstName');
+
+// firstName.addEventListener('cut', () => {
+//   console.log(firstName.value);
+// });
+
+// firstName.addEventListener('copy', () => {
+//   console.log(firstName.value);
+// });
+
+// firstName.addEventListener('paste', () => {
+//   console.log(firstName.value);
+// });
+
+// Bubbling and capturing
+// stopPropagation
+
+// const div = document.querySelector('#div');
+// const p = document.querySelector('#p');
+// const span = document.querySelector('#span');
+
+// div.addEventListener('click', () => console.log('div'));
+// p.addEventListener('click', (e) => {
+//   e.stopPropagation();
+//   console.log('p');
+// })
+// span.addEventListener('click', (e) => {
+//   console.log('span');
+// });
+
+
+
+// localStorage/sessionStorage
+// setItem(key, value)
+// getItem(key)
+// removeItem(key)
+// JSON.parse/JSON.stringify
